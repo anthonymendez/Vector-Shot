db74d1133e1c37ef10f34a920b94d3954592d02f
Anthony Mendez <anthonymendez9@gmail.com>
2017-07-02 13:14:42 -0400

    Camera follows player smoothly

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -6,14 +6,25 @@ public class Camera : MonoBehaviour {
 
     GameObject player;
 
+    Vector3 offset;
+    Vector3 velocity;
+    
+
 	// Use this for initialization
 	void Start () {
         player = GameObject.FindWithTag("Player");
+        offset = transform.position - player.transform.position;
+        velocity = Vector3.zero;
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    void Update() {
         CheckDead();
+    }
+
+    // Updates regardless of Framerate
+    void FixedUpdate () {
+        transform.position = Vector3.SmoothDamp(transform.position,player.transform.position,ref velocity,0.25f, 50.0f, Time.deltaTime);
+        transform.position = new Vector3(transform.position.x, transform.position.y, -10f);
 	}
 
     //If player is dead, we're going to end the game and bring up the menu
