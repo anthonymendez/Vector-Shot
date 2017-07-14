9b1bda9254dc1ae3a434b923c9cec01a956e0058
Anthony Mendez <anthonymendez9@gmail.com>
2017-07-14 09:47:57 -0400

    Added Credits menu to pause menu

--- Assets/Game Jam Template/Scripts/ShowPanels.cs
+++ Assets/Game Jam Template/Scripts/ShowPanels.cs
@@ -3,26 +3,26 @@ using System.Collections;
 
 public class ShowPanels : MonoBehaviour {
 
-	public GameObject optionsPanel;							//Store a reference to the Game Object OptionsPanel 
-	public GameObject optionsTint;							//Store a reference to the Game Object OptionsTint 
-	public GameObject menuPanel;							//Store a reference to the Game Object MenuPanel 
-	public GameObject pausePanel;							//Store a reference to the Game Object PausePanel 
+    public GameObject optionsPanel;							//Store a reference to the Game Object OptionsPanel 
+    public GameObject optionsTint;							//Store a reference to the Game Object OptionsTint 
+    public GameObject menuPanel;							//Store a reference to the Game Object MenuPanel 
+    public GameObject pausePanel;							//Store a reference to the Game Object PausePanel 
     public GameObject creditsPanel;                         //Store a reference to the Game Object CreditsPanel
 
 
-	//Call this function to activate and display the Options panel during the main menu
-	public void ShowOptionsPanel()
-	{
-		optionsPanel.SetActive(true);
-		optionsTint.SetActive(true);
-	}
+    //Call this function to activate and display the Options panel during the main menu
+    public void ShowOptionsPanel()
+    {
+        optionsPanel.SetActive(true);
+        optionsTint.SetActive(true);
+    }
 
-	//Call this function to deactivate and hide the Options panel during the main menu
-	public void HideOptionsPanel()
-	{
-		optionsPanel.SetActive(false);
-		optionsTint.SetActive(false);
-	}
+    //Call this function to deactivate and hide the Options panel during the main menu
+    public void HideOptionsPanel()
+    {
+        optionsPanel.SetActive(false);
+        optionsTint.SetActive(false);
+    }
 
     //Call this function to activate and display the Options panel during the main menu
     public void ShowCreditsPanel() {
@@ -36,28 +36,28 @@ public class ShowPanels : MonoBehaviour {
 
     //Call this function to activate and display the main menu panel during the main menu
     public void ShowMenu()
-	{
-		menuPanel.SetActive (true);
-	}
+    {
+        menuPanel.SetActive (true);
+    }
 
-	//Call this function to deactivate and hide the main menu panel during the main menu
-	public void HideMenu()
-	{
-		menuPanel.SetActive (false);
-	}
-	
-	//Call this function to activate and display the Pause panel during game play
-	public void ShowPausePanel()
-	{
-		pausePanel.SetActive (true);
-		optionsTint.SetActive(true);
-	}
+    //Call this function to deactivate and hide the main menu panel during the main menu
+    public void HideMenu()
+    {
+        menuPanel.SetActive (false);
+    }
+    
+    //Call this function to activate and display the Pause panel during game play
+    public void ShowPausePanel()
+    {
+        pausePanel.SetActive (true);
+        optionsTint.SetActive(true);
+    }
 
-	//Call this function to deactivate and hide the Pause panel during game play
-	public void HidePausePanel()
-	{
-		pausePanel.SetActive (false);
-		optionsTint.SetActive(false);
+    //Call this function to deactivate and hide the Pause panel during game play
+    public void HidePausePanel()
+    {
+        pausePanel.SetActive (false);
+        optionsTint.SetActive(false);
 
-	}
+    }
 }
