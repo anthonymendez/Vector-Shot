cf3f82294ca4567826b38fa910ba274227c55d56
Anthony Mendez <anthonymendez9@gmail.com>
2018-05-30 10:32:40 -0400

    Removed main level. Refactored Key.cs

--- Assets/Scripts/Environment/Key.cs
+++ Assets/Scripts/Environment/Key.cs
@@ -3,33 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Key : MonoBehaviour {
-
-    public GameObject[] ignoreCollisions;
-
-    public GameObject UI;
-
-    GameObjectPool keyPool;
+    
+    [SerializeField] GameObject UI;
+    [SerializeField] GameObjectPool keyPool;
+    [SerializeField] int pointsPerPickup = 50;
     AudioSource keyPickupSound;
 
     private void Start() {
         keyPool = GameObject.FindWithTag("KeyPool").GetComponent<GameObjectPool>();
         keyPickupSound = GetComponent<AudioSource>();
-        foreach (GameObject gObj in ignoreCollisions) {
-
-        }
         UI = GameObject.FindWithTag("UI");
     }
 
     void OnCollisionEnter2D(Collision2D collision) {
-        if (collision.gameObject.CompareTag("Player")){
-            Variables.score += 50;
-            Variables.keysObtained++;
-            AudioSource.PlayClipAtPoint(keyPickupSound.clip,transform.position);
-            keyPool.AddGameObject(gameObject);
-            if(Variables.keysObtained == 8) {
-                UI.GetComponentInChildren<WinMenu>().WMUI.SetActive(true);
-                Time.timeScale = 0;
-            }
+        if (collision.gameObject.CompareTag("Player")) {
+            GivePlayerPoints();
+            CheckIfWin();
+        }
+    }
+
+    private void GivePlayerPoints() {
+        Variables.score += pointsPerPickup;
+        Variables.keysObtained++;
+        AudioSource.PlayClipAtPoint(keyPickupSound.clip, transform.position);
+        keyPool.AddGameObject(gameObject);
+    }
+
+    private void CheckIfWin() {
+        if (Variables.keysObtained == 8) {
+            UI.GetComponentInChildren<WinMenu>().WMUI.SetActive(true);
+            Time.timeScale = 0;
         }
     }
 }
